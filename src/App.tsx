71cc602621d6b7dee72bb96edde37c24b1f8e41b
AdamMorris <adam.n.morris@target.com>
2020-01-07 16:28:30 -0600

    Added glTF logo and GitHub ribbon.

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,27 @@ import "./App.css";
 import ProjectList from "./components/ProjectList";
 import FilterBar from "./components/FilterBar";
 import SearchBar from "./components/SearchBar";
+import GitHubForkRibbon from "react-github-fork-ribbon";
 
 const App: React.FC = () => {
   return (
     <>
       <div className="header">
-        <h1>glTF Project Explorer</h1>
+        <img src="/glTF_RGB_June16.svg" alt="glTF" />
+        <h1>Project Explorer</h1>
       </div>
       <div className="content">
         <SearchBar></SearchBar>
         <FilterBar></FilterBar>
         <ProjectList></ProjectList>
       </div>
+      <GitHubForkRibbon
+        position="right"
+        color="white"
+        href="https://github.com/KhronosGroup/glTF-Project-Explorer"
+      >
+        Fork us on GitHub
+      </GitHubForkRibbon>
     </>
   );
 };
