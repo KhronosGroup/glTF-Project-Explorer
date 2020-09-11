004e93cd36cf8fb2674d21d4d612805852f74c4c
Adam.N.Morris <adam.n.morris@target.com>
2020-09-11 09:43:17 -0500

    Fixing public relative paths.

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -3,7 +3,7 @@ import { IProjectInfo } from "../interfaces/IProjectInfo";
 // Despite the data being a static file, we don't pull it in using Webpack so
 //   we can change to using a restful service in the future.
 export function fetchProjects(): Promise<IProjectInfo[]> {
-  return fetch("./data/glTF-projects-data.json")
+  return fetch(`${process.env.PUBLIC_URL}/data/glTF-projects-data.json`)
     .then(r => r.json())
     .catch(error => console.error(`Error fetching data. Reason: ${error}`));
 }
