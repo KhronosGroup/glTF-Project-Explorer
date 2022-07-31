3335dae37de55fa4b338b610280776857471c7aa
Marco Hutter <javagl@javagl.de>
2022-07-31 14:40:10 +0200

    Draft of adding a properties record to IProjectInfo

--- src/interfaces/IProjectInfo.ts
+++ src/interfaces/IProjectInfo.ts
@@ -6,11 +6,24 @@ export type ProjectLicense = string;
 export type ProjectTag = string;
 export type ProjectExtension = string;
 
+/**
+ * This COULD already be an entity description generated using
+ * https://github.com/typeorm/typeorm, but 
+ * - we don't know yet how persistence will be implemented
+ * - the entities have to be dynamic in terms of the "columns"
+ * Instances of this class could probably be mapped to
+ * actual TypeORM entities in the persistence layer, while
+ * the UI solely operates on the IProjectInfo.
+ */
+
 export interface IProjectInfo {
   id: number;
   name: string;
   description?: string;
   link?: string;
+
+  properties: Record<string, string[]>;
+
   task?: ProjectTask[];
   license?: ProjectLicense[];
   type?: ProjectType[];
--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -2,24 +2,48 @@ import { IProjectInfo } from "../interfaces/IProjectInfo";
 
 // Despite the data being a static file, we don't pull it in using Webpack so
 //   we can change to using a restful service in the future.
-export function fetchProjects(): Promise<IProjectInfo[]> {
+function fetchProjectsInternal(): Promise<IProjectInfo[]> {
   return fetch(`${process.env.PUBLIC_URL}/data/glTF-projects-data.json`)
     .then((r) => r.json())
     .catch((error) => console.error(`Error fetching data. Reason: ${error}`));
 }
 
-export function fetchProjectsWithId(): Promise<IProjectInfo[]> {
-  return new Promise(async (resolve) => {
-    const projects = await fetchProjects();
+/**
+ * Converts the given project info into the new structure, where the
+ * properties are stored in the `properties` record.
+ * 
+ * TODO_GENERALIZATION This is a preliminary state. In the future,
+ * the input JSON could be modified, or be represented by a structure
+ * like "ILegacyProjectInfo" that is converted into the IProjectInfo
+ * here. 
+ * 
+ * @param p The project info
+ * @returns The given project info
+ */
+function convertProjectInfo(p : IProjectInfo) : IProjectInfo {
+  p.properties = {};
+  p.properties["task"] = p.task ? p.task : [];
+  p.properties["license"] = p.license ? p.license : [];
+  p.properties["type"] = p.type ? p.type : [];
+  p.properties["language"] = p.language ? p.language : [];
+  p.properties["inputs"] = p.inputs ? p.inputs : [];
+  p.properties["outputs"] = p.outputs ? p.outputs : [];
+  p.properties["tags"] = p.tags ? p.tags : [];
+  return p;
+}
 
+export function fetchProjects(): Promise<IProjectInfo[]> {
+  return new Promise(async (resolve) => {
+    const projects = await fetchProjectsInternal();
+    
+    // This work gives us a stable key. Eventually when this is database
+    // backed the ID will be provided by the DB and this can be removed.
     let id = 0;
-    resolve(
-      projects.map((p) => {
-        // This work gives us a stable key. Eventually when this is database
-        // backed the ID will be provided by the DB and this can be removed.
-        p.id = id++;
-        return p;
-      })
-    );
+    let resultProjects = projects.map((p) => {
+      p.id = id++;
+      return p;
+    });
+    resultProjects = resultProjects.map(convertProjectInfo);
+    resolve(resultProjects)
   });
 }
